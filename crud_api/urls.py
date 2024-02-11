c1a9b9e7ade66eed5689b96a7edb0d2c20127bf6
Utkarsh Raj <utkarshraj432k@gmail.com>
2024-02-11 21:45:38 +0530

    upd: Updated url name

--- crud_api/urls.py
+++ crud_api/urls.py
@@ -4,5 +4,5 @@ from .views import CreateBook,RetrieveUpdateDestroyBook
 
 urlpatterns = [
     path('book/', CreateBook.as_view(),name="create_book"),
-    path('update/<int:pk>/',RetrieveUpdateDestroyBook.as_view(),name="retrieve_update_destroy_book"),
+    path('book/<int:pk>/',RetrieveUpdateDestroyBook.as_view(),name="retrieve_update_destroy_book"),
 ]
