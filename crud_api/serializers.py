f199a7cb15e68c96a074359bc14cdd87d368e5d8
Utkarsh Raj <utkarshraj432k@gmail.com>
2024-02-22 14:18:24 +0530

    new: serializers validation

--- crud_api/serializers.py
+++ crud_api/serializers.py
@@ -1,7 +1,20 @@
+import re
 from rest_framework import serializers
 from .models import Book
+from django.utils import timezone
+
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate(self, data):
+        if data['publication_date'] > timezone.now().date():
+            raise serializers.ValidationError("Date cannot be in future")
+        
+        pattern = r'^[a-zA-Z]'
+        title = data['author']
+        if not re.search(pattern, title):
+            raise serializers.ValidationError("Do not use any special character")
+        return data
